89d4ff27add04161ebed0f59a1bcf30bfed486b9
Reg Stuart <reg.stuart@alico.com>
2012-06-25 16:54:59 +0100

    error checking.

--- php_includes/upload.inc.php
+++ php_includes/upload.inc.php
@@ -1,7 +1,7 @@
 <?php
 $maxFileSize = 512000;					// max file size in bytes
 $destination = 'c:/xuploads/';  		//define the path to the upload folder
-$permittedFileTypes = array('image/gif', 'image/jpeg', 'image/pjpeg', 'image/png', 'text/plain');
+$permittedFileTypes = array('image/gif', 'image/jpeg', 'image/pjpeg', 'image/png', 'text/plain', 'text/log');
 
 if ( isset( $_POST['upload'] ) ) {
 
@@ -45,15 +45,68 @@ class Rsc_Upload {
 	 */  
 	public function move() {
 		$field = current($this->_uploaded);
-		$success = move_uploaded_file($field['tmp_name'], $this->_destination . $field['name']);
-		if ($success) {
-			$this->_messages[] = $field['name'] . ' uploaded successfully';
-		} else {
-			$this->_messages[] = 'Could not upload ' . $field['name'];
-		}		
+		$OK = $this->checkError($field['name'], $field['error']);
+		if ( $OK ) {
+			$sizeOK = $this->checkSize($field['name'], $field['size']);
+			$typeOK = $this->checkType($field['name'], $field['type']);
+			$success = move_uploaded_file($field['tmp_name'], $this->_destination . $field['name']);		
+			if ($success) {
+				$this->_messages[] = $field['name'] . ' uploaded successfully';
+			} else {
+				$this->_messages[] = 'Could not upload ' . $field['name'];
+			}		
+		}
 	}
 	
 	public function getMessages() {
 		return $this->_messages;
+	}
+	
+	protected function checkError($filename, $error) {
+		switch ($error) {
+			case 0: {
+						return true;
+					}
+			case 1:
+			case 2: {
+						$this->_messages[] = "$filename exceeds maximum size: ". $this->getMaxSize();
+						return true;
+					}
+			case 3: {
+						$this->_messages[] = "Error uploading $filename.  Please try again.";
+						return false;
+					}
+			case 4: {
+						$this->_messages[] = "No file selected.";
+						return false;
+					}
+			default: 	{
+							$this->_messages[] = "System error uploading $filename.  Contact webmaster.";
+							return false;
+						}
+		}
+	}
+	
+	protected function checkSize($filename, $size) {
+		if ($size==0) {
+			return false;
+		} elseif ($size > $this->_maxFileSize) {
+			$this->_messages[] = "$filename exceeds maximum size: " . $this->getMaxSize();
+			return false;
+		} else {
+			return true;
+		}
+	}
+	
+	protected function checkType($filename, $type) { 
+		if ( !in_array($type, $this->_permittedFileTypes )) {
+			$this->_messages[] = "$filename is not a permitted type of file.";
+			return false;
+		} else {
+			return true;
+		}
+	}
+	public function getMaxSize() {
+		return number_format($this->_maxFileSize / 1024, 1) . 'kB';
 	}
 }
